fb356f44130e135096c61c7ff46d4402df1216f8
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-11-07 15:59:09 +0100

    Add Home Page

---
+++ app/src/main/java/shagold/wifwaf/HomeActivity.java
@@ -0,0 +1,31 @@
+package shagold.wifwaf;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import shagold.wifwaf.manager.MenuManager;
+
+/**
+ * Created by jimmy on 07/11/15.
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_default, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return MenuManager.defaultMenu(this, item.getItemId()) || super.onOptionsItemSelected(item);
+    }
+}
