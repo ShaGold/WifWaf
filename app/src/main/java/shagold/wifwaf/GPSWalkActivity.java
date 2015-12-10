65b1d61ce171b1dce7ffdd6155d7ea8cb8bc168e
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-12-10 16:11:31 +0100

    Mise en place de la carte de base

--- app/src/main/java/shagold/wifwaf/GPSWalkActivity.java
+++ app/src/main/java/shagold/wifwaf/GPSWalkActivity.java
@@ -1,21 +1,81 @@
 package shagold.wifwaf;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import shagold.wifwaf.manager.MenuManager;
 
 /**
  * Created by jimmy on 22/11/15.
  */
-public class GPSWalkActivity extends AppCompatActivity {
+public class GPSWalkActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    LocationManager locationManager;
+    Location mCurrentLocation;
+    private GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    Marker mo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps_walk);
+
+        setUpMapIfNeeded();
+
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+
+        buildGoogleApiClient();
+
+        mGoogleApiClient.connect();
+
+        createLocationRequest();
+
+        LocationRequest mLocationRequest;
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+        }
+
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+
+
     }
 
     @Override
@@ -27,5 +87,102 @@ public class GPSWalkActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return MenuManager.defaultMenu(this, item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        mo = mMap.addMarker(new MarkerOptions().position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude())).title("Gold"));
+
+        Log.d("TT", "onCreate ..............................." + mLastLocation.toString());
+
+        if (true) {
+            startLocationUpdates();
+        }
+
+    }
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+
+        if(mo != null) {
+            LatLng ll = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+            Log.d("POS", "MOVE ..............................." + ll.toString());
+            mo.setPosition(ll);
+            /*lines.add(ll);
+            linesLatLng.add(ll);
+
+            if(linesLatLng.size() > 2) { // TODO remove pl
+                PolylineOptions temp = new PolylineOptions()
+                        .add(linesLatLng.getLast())
+                        .add(linesLatLng.get(linesLatLng.size() - 2));
+
+                temp.color(Color.BLUE);
+                temp.visible(true);
+                temp.width(10);
+                pl.add(temp);
+                Polyline t = mMap.addPolyline(temp);
+            }*/
+        }
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+
+        Log.d("TTT", "Start ...............................");
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 }
