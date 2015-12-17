4e1e04fe7813303a8c5febf7e130948fc658ee5b
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-12-17 14:44:48 +0100

    Ajout choix de l heure et de la date pour une balade

--- app/src/main/java/shagold/wifwaf/UseWalkActivity.java
+++ app/src/main/java/shagold/wifwaf/UseWalkActivity.java
@@ -1,16 +1,25 @@
 package shagold.wifwaf;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import shagold.wifwaf.dataBase.Walk;
 import shagold.wifwaf.manager.MenuManager;
+import shagold.wifwaf.tool.WifWafDatePickerFragment;
+import shagold.wifwaf.tool.WifWafTimePickerFragment;
 
 /**
  * Created by jimmy on 17/12/15.
  */
 public class UseWalkActivity extends AppCompatActivity {
+
+    private Walk walk;
 
 
     @Override
@@ -18,7 +27,18 @@ public class UseWalkActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_use_walk);
 
+        walk = (Walk) getIntent().getSerializableExtra("WALK");
+        initUseWalk();
 
+    }
+
+    private void initUseWalk() {
+        if(walk != null) {
+            EditText title = (EditText) findViewById(R.id.nameUseWalk);
+            title.setText(walk.getTitle());
+            EditText description = (EditText) findViewById(R.id.descriptionUseWalk);
+            description.setText(walk.getDescription());
+        }
     }
 
     @Override
@@ -30,6 +50,20 @@ public class UseWalkActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return MenuManager.defaultMenu(this, item) || super.onOptionsItemSelected(item);
+    }
+
+    public void choseTimeStamp(View view) {
+        WifWafTimePickerFragment newFragment = new WifWafTimePickerFragment();
+        TextView textView = (TextView) findViewById(R.id.timeStampUseWalk);
+        newFragment.setTimeText(textView);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void choseDate(View view) {
+        WifWafDatePickerFragment newFragment = new WifWafDatePickerFragment();
+        TextView textView = (TextView) findViewById(R.id.dateUseWalk);
+        newFragment.setDateText(textView);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
 }
