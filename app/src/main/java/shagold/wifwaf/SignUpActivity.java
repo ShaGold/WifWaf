060c857c3c157701c5b3f9439d82516cc7d88e7b
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-12-20 21:54:42 +0100

    Clean menu activity

--- app/src/main/java/shagold/wifwaf/SignUpActivity.java
+++ app/src/main/java/shagold/wifwaf/SignUpActivity.java
@@ -46,8 +46,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_empty, menu);
+        getMenuInflater().inflate(R.menu.menu_default, menu);
         return true;
     }
 
