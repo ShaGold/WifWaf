a7f2c1b96a6e7c2916fdb957e39c0c7325c5b5cd
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2016-01-04 15:36:50 +0100

    Ajout police perso

--- app/src/main/java/shagold/wifwaf/MainActivity.java
+++ app/src/main/java/shagold/wifwaf/MainActivity.java
@@ -3,6 +3,7 @@ package shagold.wifwaf;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import shagold.wifwaf.dataBase.User;
 import shagold.wifwaf.manager.MenuManager;
@@ -55,6 +57,11 @@ public class MainActivity extends AppCompatActivity {
         mSocket = SocketManager.getMySocket();
         mSocket.connect();
         mSocket.on("RTrySignIn", onRTrySignIn);
+
+        //Police perso sur le titre
+        TextView text = (TextView) findViewById(R.id.titreappli);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/JennaSue.ttf");
+        text.setTypeface(tf);
     }
 
     @Override
